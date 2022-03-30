6a783c5002c20f4f7b09c39068cb98e6f4bc645f
spalme21 <spalme21@gmail.com>
2022-03-30 14:53:45 -0500

    punch card app, first commit

---
+++ punch_card/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PunchCardConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'punch_card'
---
+++ punch_card/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Client(models.Model):
+    """A client"""
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    phone = models.CharField(max_length=15)
+    email = models.EmailField(max_length=254)
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        return f'{self.first_name} {self.last_name}'
